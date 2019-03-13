2f811919834b61fd7145c7693dc553e089149e92
Brian Leighton <bleighton@acadian-asset.com>
2019-03-12 20:46:06 -0400

    added bottom tabs

--- src/components/DefaultHeader/index.js
+++ src/components/DefaultHeader/index.js
@@ -1,34 +1,21 @@
 const React = require('react');
 const T = require('prop-types');
-const { Header, Left, Body, Right, Button, Icon, Title } = require('native-base');
+const { Header, Body, Right, Button, Icon, Title } = require('native-base');
 
 module.exports = class DefaultHeader extends React.PureComponent {
 
     static propTypes = {
-        goBack: T.func,
         openMenu: T.func,
         title: T.string
     };
 
     render() {
 
-        const { goBack, title, openMenu } = this.props;
+        const { title, openMenu } = this.props;
 
         return (
 
             <Header {...this.props}>
-                <Left>
-                    {goBack &&
-                        <Button transparent onPress={() => goBack()}>
-                            <Icon name='arrow-back' />
-                        </Button>
-                    }
-                    {!goBack &&
-                    <Button transparent>
-                        <Icon name='home' />
-                    </Button>
-                    }
-                </Left>
                 <Body>
                     <Title>{title}</Title>
                 </Body>
--- src/screens/home/components/HomeView.js
+++ src/screens/home/components/HomeView.js
@@ -1,9 +1,9 @@
 const React = require('react');
 const T = require('prop-types');
-const { CardItem } = require('native-base');
-const { Card, Container, Text } = require('styles');
+const { Container, Content, Card, CardItem, Footer, FooterTab, Text, Button, Icon } = require('native-base');
+// const { ,  } = require('styles');
 const { Joystick } = require('joystick-component-lib');
-const { Button } = require('styles');
+// const {  } = require('styles');
 
 module.exports = class HomeView extends React.PureComponent {
 
@@ -20,6 +20,7 @@ module.exports = class HomeView extends React.PureComponent {
         super(props);
 
         this.state = {
+            view: 'beer',
             px: 0,
             py: 0,
             dx: 0,
@@ -50,38 +51,69 @@ module.exports = class HomeView extends React.PureComponent {
         });
     }
 
+    renderBeerInfo() {
+
+        return (
+            <Card>
+                <CardItem header bordered>
+                    <Text>Drink Beer!</Text>
+                </CardItem>
+            </Card>
+        );
+    }
+
+    renderDrive() {
+
+        return (
+            <Card>
+                <CardItem header bordered>
+                    <Text>Drive ({this.props.isConnected ? 'connected' : 'not connected'})</Text>
+                </CardItem>
+                <CardItem bordered>
+                    <Text>Battery: {this.props.battery}</Text>
+                    <Button
+                        onPress={this.props.isConnected ? this.props.disconnect : this.props.connect}
+                        block
+                        rounded
+                    >
+                        <Text>{this.props.isConnected ? 'Disconnect' : 'Connect'}</Text>
+                    </Button>
+                </CardItem>
+                <CardItem bordered>
+                    <Container>
+                        <Joystick
+                            shape='circular'
+                            length={100}
+                            neutralPointX={100}
+                            neutralPointY={100}
+                            onDraggableMove={this.handleDrag}
+                            onDraggableRelease={this.handleDragRelease}
+                            isSticky
+                        />
+                    </Container>
+                </CardItem>
+            </Card>
+        );
+    }
+
     render() {
 
         return (
 
             <Container>
-                <Card>
-                    <CardItem header bordered>
-                        <Text>Drive ({this.props.isConnected ? 'connected' : 'not connected'})</Text>
-                    </CardItem>
-                    <CardItem bordered>
-                        <Text>Battery: {this.props.battery}</Text>
-                        <Button
-                            onPress={this.props.isConnected ? this.props.disconnect : this.props.connect}
-                            text={this.props.isConnected ? 'Disconnect' : 'Connect'}
-                            block
-                            rounded
-                        />
-                    </CardItem>
-                    <CardItem bordered>
-                        <Container>
-                            <Joystick
-                                shape='circular'
-                                length={100}
-                                neutralPointX={100}
-                                neutralPointY={100}
-                                onDraggableMove={this.handleDrag}
-                                onDraggableRelease={this.handleDragRelease}
-                                isSticky
-                            />
-                        </Container>
-                    </CardItem>
-                </Card>
+                <Content scrollEnabled={false}>
+                    {this.state.view === 'beer' ? this.renderBeerInfo() : this.renderDrive()}
+                </Content>
+                <Footer>
+                    <FooterTab>
+                        <Button active={this.state.view === 'beer'} onPress={() => this.setState({ view: 'beer' })}>
+                            <Icon name='beer' />
+                        </Button>
+                        <Button active={this.state.view === 'drive'} onPress={() => this.setState({ view: 'drive' })}>
+                            <Icon active name='car' />
+                        </Button>
+                    </FooterTab>
+                </Footer>
             </Container>
         );
     }
