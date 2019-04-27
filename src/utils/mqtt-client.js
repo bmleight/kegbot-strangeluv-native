1f180c5c595ceb0a5e422276cce144e8246a1bf6
Brian Leighton <bleighton@acadian-asset.com>
2019-04-26 23:59:35 -0400

    forgot one

--- src/utils/mqtt-client.js
+++ src/utils/mqtt-client.js
@@ -1,9 +1,9 @@
 const { Client } = require('react-native-paho-mqtt');
 
 const internals = {
-    // host: process.env.API_HOST || 'ws://192.168.86.37:9001/ws',
+    host: process.env.API_HOST || 'ws://192.168.86.37:9001/ws',
     // host: process.env.API_HOST || 'ws://127.0.0.1:9001/ws',
-    host: process.env.API_HOST || 'ws://192.168.2.128:9001/ws',
+    // host: process.env.API_HOST || 'ws://192.168.2.128:9001/ws',
     storage: {  //Set up an in-memory alternative to global localStorage
         setItem: (key, item) => {
 
